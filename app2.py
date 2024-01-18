bb8190c28a025a7c1ce319a418a18cfb6f41286a
Michael Ye <michaelye22@gmail.com>
2024-01-18 17:41:59 -0500

    feat: initialize app2, which is where I learn how to integrate HTML to streamlit

---
+++ app2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+# Load HTML file
+with open('Titanic_Website_V2\index.html', 'r') as file:
+    html_content = file.read()
+
+# Display HTML
+st.markdown(html_content, unsafe_allow_html=True)
