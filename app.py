8af373f0ef793a034e72332d0869a61c47f31e8c
Michael Ye <michaelye22@gmail.com>
2024-01-19 15:18:15 -0500

    feat: remove the prediction from app.py

--- app.py
+++ app.py
@@ -25,47 +25,4 @@ with col2:
 with col3:
     st.write("")
 
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
-st.write("")
 
-model12 = load_model("model_12_saved.h5")
-
-
-# User input for each feature
-pclass = st.selectbox('Ticket class: 1 = 1st, 2 = 2nd, 3 = 3rd', [1, 2, 3])
-sex = st.selectbox('Sex', ['male', 'female'])
-fare = st.slider('Fare', min_value=0.0, max_value=50.0, value=25.0, step=0.5)
-age = st.slider('Age', min_value=0.0, max_value=100.0, value=30.0, step=1.0)
-sibsp = st.selectbox('Number of Siblings/Spouses Aboard', [0, 1, 2])
-parch = st.selectbox('Number of Parents/Children Aboard', [0, 1, 2])
-
-
-def predict(pclass, sex, fare, age, sibsp, parch):
-    # Convert inputs to model's expected format
-    pclass = int(pclass)
-    sex = str(sex)
-    fare = float(fare)
-    age = float(age)
-    sibsp = int(sibsp)
-    parch = int(parch)
-
-
-    # Prepare the input data in the correct format
-
-    preprocessed_data = data_preprocessing(pclass, sex, fare, age, sibsp, parch)
-
-    # Make prediction
-    prediction = model12.predict(preprocessed_data)
-    
-    return prediction
-
-
-if st.button('Predict'):
-    prediction_float = predict(pclass, sex, fare, age, sibsp, parch)
-    prediction_label = "Survived" if prediction_float > 0.5 else "Not Survived"
-    st.write(f'Prediction: {prediction_label} ({prediction_float})')
-    
